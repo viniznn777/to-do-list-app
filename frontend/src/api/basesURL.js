a35746b04cbd591cbf9c2f5080fde553c66d6327
Vinícius Kauã <viniciuskauan30@hotmail.com>
2024-04-09 23:20:23 -0300

    v1

--- frontend/src/api/basesURL.js
+++ frontend/src/api/basesURL.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+// https://to-do-list-app-zl5u.onrender.com
+
 export const AUTH_URL = axios.create({
-  baseURL: "http://localhost:8081/api/auth",
+  baseURL: "https://to-do-list-app-zl5u.onrender.com/api/auth",
   // Como default o axios só vem configurado para tratar status de maior que 200 e até 300
   // Então foi definido para tratamentos de status NESTA ROTA de até 404
   validateStatus: function (status) {
@@ -9,7 +11,7 @@ export const AUTH_URL = axios.create({
   },
 });
 export const MANAGE_TASK = axios.create({
-  baseURL: "http://localhost:8081/manage",
+  baseURL: "https://to-do-list-app-zl5u.onrender.com/manage",
   // Como default o axios só vem configurado para tratar status de maior que 200 e até 300
   // Então foi definido para tratamentos de status NESTA ROTA de até 404
   validateStatus: function (status) {
@@ -17,7 +19,7 @@ export const MANAGE_TASK = axios.create({
   },
 });
 export const MANAGE_USER = axios.create({
-  baseURL: "http://localhost:8081/user/private_info",
+  baseURL: "https://to-do-list-app-zl5u.onrender.com/user/private_info",
   // Como default o axios só vem configurado para tratar status de maior que 200 e até 300
   // Então foi definido para tratamentos de status NESTA ROTA de até 404
   validateStatus: function (status) {
